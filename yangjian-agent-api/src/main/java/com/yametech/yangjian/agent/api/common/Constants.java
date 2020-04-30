b5a88b7cbfaedc179150dab1dd7e2fafdfc81817
dengliming <liming.d.pro@gmail.com>
2020-04-30 16:51:24 +0800

    Kafka链路插件适配

--- yangjian-agent-api/src/main/java/com/yametech/yangjian/agent/api/common/Constants.java
+++ yangjian-agent-api/src/main/java/com/yametech/yangjian/agent/api/common/Constants.java
@@ -92,6 +92,8 @@ public class Constants {
         public static final String STATUS_CODE = "status_code";
         public static final String URL = "url";
         public static final String HTTP_METHOD = "http.method";
-        public static final String KAFKA_TOPIC = "kafka.topic";
+        public static final String MQ_TOPIC = "mq.topic";
+        public static final String MQ_SERVER = "mq.broker";
+        public static final String MQ_CONSUMER = "mq.consumer";
     }
 }
