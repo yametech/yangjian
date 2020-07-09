ae84e29c5e5243631258d06be4c858e39092a7d8
dengliming <liming.d.pro@gmail.com>
2020-07-09 16:59:26 +0800

    增加zipkin插件解决spanname被强制转小写问题

---
+++ yangjian-agent-plugins/agent-plugin-zipkin/src/main/java/com/yametech/yangjian/agent/plugin/zipkin/SpanBuilderInterceptor.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2020 yametech.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.yametech.yangjian.agent.plugin.zipkin;
+
+import com.yametech.yangjian.agent.api.base.IContext;
+import com.yametech.yangjian.agent.api.bean.BeforeResult;
+import com.yametech.yangjian.agent.api.interceptor.IMethodAOP;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import static com.yametech.yangjian.agent.plugin.zipkin.context.ContextConstants.SPAN_NAME_KEY;
+
+/**
+ * @author dengliming
+ * @date 2020/7/9
+ */
+public class SpanBuilderInterceptor implements IMethodAOP {
+
+    @Override
+    public BeforeResult before(Object thisObj, Object[] allArguments, Method method) throws Throwable {
+        return null;
+    }
+
+    @Override
+    public Object after(Object thisObj, Object[] allArguments, Method method, BeforeResult beforeResult, Object ret, Throwable t, Map globalVar) throws Throwable {
+        if (ret instanceof IContext && thisObj instanceof IContext) {
+            ((IContext) ret)._setAgentContext(SPAN_NAME_KEY, ((IContext) thisObj)._getAgentContext(SPAN_NAME_KEY));
+        }
+        return ret;
+    }
+}
