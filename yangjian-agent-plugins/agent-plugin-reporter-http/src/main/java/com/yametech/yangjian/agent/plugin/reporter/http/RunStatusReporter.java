60ed3bfa735b2e11187701ba019cc339eb6bf5c3
dengliming <liming.d.pro@gmail.com>
2020-08-19 12:04:34 +0800

    支持服务运行状态http上报

--- yangjian-agent-plugins/agent-plugin-reporter-http/src/main/java/com/yametech/yangjian/agent/plugin/reporter/http/RunStatusReporter.java
+++ yangjian-agent-plugins/agent-plugin-reporter-http/src/main/java/com/yametech/yangjian/agent/plugin/reporter/http/RunStatusReporter.java
@@ -13,38 +13,48 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.yametech.yangjian.agent.plugin.reporter.http;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+package com.yametech.yangjian.agent.plugin.reporter.http;
 
 import com.yametech.yangjian.agent.api.IConfigReader;
 import com.yametech.yangjian.agent.api.IReport;
-import com.yametech.yangjian.agent.api.bean.ConfigNotifyType;
-import com.yametech.yangjian.agent.api.common.Constants;
 import com.yametech.yangjian.agent.api.common.StringUtil;
 import com.yametech.yangjian.agent.util.HttpClient;
 import com.yametech.yangjian.agent.util.HttpRequest;
 import com.yametech.yangjian.agent.util.HttpResponse;
 
+import java.util.*;
+
 /**
- * HTTP上报
- * <p>
- * 注：agent.properties配置report=http
- *
  * @author dengliming
- * @date 2020/3/5
+ * @date 2020/8/17
  */
-public class HttpReporter implements IReport, IConfigReader {
-    private static final String URL_CONFIG_KEY = "report.http-span.url";
-    private static final String SPAN_REPORT_TYPE = "http-span";
+public class RunStatusReporter implements IReport, IConfigReader {
+
+    private static final String URL_CONFIG_KEY = "report.http-status.url";
+
+    private static final String REPORT_TYPE = "http-status";
     /**
      * 上报的URL
      */
     private String url;
+
+    @Override
+    public Set<String> configKey() {
+        return new HashSet<>(Arrays.asList(URL_CONFIG_KEY.replaceAll("\\.", "\\\\.")));
+    }
+
+    @Override
+    public void configKeyValue(Map<String, String> kv) {
+        if (kv.containsKey(URL_CONFIG_KEY)) {
+            url = kv.get(URL_CONFIG_KEY);
+        }
+    }
+
+    @Override
+    public String type() {
+        return REPORT_TYPE;
+    }
 
     @Override
     public boolean report(Object data) {
@@ -67,27 +77,4 @@ public class HttpReporter implements IReport, IConfigReader {
         }
         return true;
     }
-
-    @Override
-    public String type() {
-        return SPAN_REPORT_TYPE;
-    }
-
-    @Override
-    public Set<String> configKey() {
-        return new HashSet<>(Arrays.asList(URL_CONFIG_KEY.replaceAll("\\.", "\\\\.")));
-    }
-
-    @Override
-    public void configKeyValue(Map<String, String> kv) {
-        if (kv.containsKey(URL_CONFIG_KEY)) {
-            url = kv.get(URL_CONFIG_KEY);
-        }
-    }
-
-    @Override
-    public ConfigNotifyType notifyType() {
-        return ConfigNotifyType.ALWAYS;
-    }
-
 }
